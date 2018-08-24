cfd1797f81e057dd5cc12801e385f629dd3638fe
Ubuntu <lacjk217@gmail.com>
2018-08-24 05:58:20 +0000

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello 20180824!");
+    response.end("Hello world 20180824!");
 
 });
 
